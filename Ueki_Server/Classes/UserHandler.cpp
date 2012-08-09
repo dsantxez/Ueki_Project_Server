f94eae6672539cc319765410ab9d76df1246abb6
denis <darzalluz@gmail.com>
2012-08-09 14:30:43 -0400

    Denis: Added Qt Project status and Netbeans 7.2 project configuration files. Created the folder structure.

---
+++ Ueki_Server/Classes/UserHandler.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   UserHandler.cpp
+ * Author: denis
+ * 
+ * Created on August 9, 2012, 2:29 PM
+ */
+
+#include "UserHandler.h"
+
+UserHandler::UserHandler() {
+}
+
+UserHandler::UserHandler(const UserHandler& orig) {
+}
+
+UserHandler::~UserHandler() {
+}
+
---
+++ Ueki_Server/Classes/UserHandler.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   UserHandler.h
+ * Author: denis
+ *
+ * Created on August 9, 2012, 2:29 PM
+ */
+
+#ifndef USERHANDLER_H
+#define	USERHANDLER_H
+
+class UserHandler {
+public:
+    UserHandler();
+    UserHandler(const UserHandler& orig);
+    virtual ~UserHandler();
+private:
+
+};
+
+#endif	/* USERHANDLER_H */
+
